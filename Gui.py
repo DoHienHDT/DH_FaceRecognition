4e5a8a22ef77726e7c6744dbee1dc7f28ae24242
dohien <dohien11091999@gmail.com>
2021-01-09 13:40:47 +0700

    add scrollview

--- Gui.py
+++ Gui.py
@@ -2,6 +2,7 @@ from tkinter import *
 import cv2
 import numpy as np
 from PIL import Image, ImageTk
+import time
 from scrollimage import ScrollableImage
 
 root = Tk()
@@ -10,6 +11,27 @@ root.configure(bg="white")
 # Button(root, text="Take Snapshot", font=("times new roman", 20, 'bold'), bg="black",fg="red").pack()
 f1 = LabelFrame(root, bg="red")
 f1.pack(side=RIGHT)
+
+text = Text(root, wrap="none")
+vsb = Scrollbar(orient="vertical", command=text.yview)
+text.configure(yscrollcommand=vsb.set)
+vsb.pack(side=LEFT, fill="y")
+text.pack(side=LEFT, expand=True)
+
+for i in range(20):
+    b =  Button(root, text="Button #%s" % i, highlightbackground='#3E4149')
+    text.window_create("end", window=b, pady=10)
+    text.insert("end", "\n")
+
+text.configure(state="disabled")
+
+def add_timestamp(self):
+    fully_scrolled_down = self.text.yview()[1] == 1.0
+    self.text.insert("end", time.ctime() + "\n")
+    if fully_scrolled_down:
+        self.text.see("end")
+    self.after(1000, self.add_timestamp)
+
 
 L1 = Label(f1, bg="red")
 L1.pack()
